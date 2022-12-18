250559fb5fc8bfda83f5a64eb6362e4d9737c556
Vasktv <maxmin05@mail.ru>
2022-12-18 18:29:47 +0500

    Домашнее задание, задача №4

---
+++ house_4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число, нажмите Enter");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число, нажмите Enter");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число, нажмите Enter");
+
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if(b>max) max=b;
+
+if(c>max) max=c;
+
+Console.WriteLine("Больщее число из введенных");
+
+System.Console.WriteLine(max);
