4693c440bdf5e36b221889ab0f19e56d67f30cd6
Vasktv <maxmin05@mail.ru>
2022-12-18 18:18:16 +0500

    Домашняя задача №2

---
+++ house_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+  System.Console.WriteLine(a); 
+
+  Console.WriteLine("Это большее число из введенных."); 
+
+}
+else    
+{
+   System.Console.WriteLine(b);  
+   Console.WriteLine("Это большее число из введенных."); 
+} 
