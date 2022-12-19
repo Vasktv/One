ee010d2e010df35552a24d5a5d1d2cc237ec2314
Vasktv <maxmin05@mail.ru>
2022-12-19 17:12:12 +0500

    Алгоритм определения четности числа

---
+++ house_6/Program.cs
@@ -0,0 +1,16 @@
+﻿int a;
+
+System.Console.WriteLine("Введите число и нажмите Enter");
+
+a = Console.Read();
+
+if (a % 2==0)
+
+System.Console.WriteLine("Чётное число");
+
+else
+{
+    System.Console.WriteLine("Не чётное число");
+}
+
+
