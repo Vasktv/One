95adb39ab9e09465c534b89faee32b9f410cd536
Vasktv <maxmin05@mail.ru>
2022-12-20 19:35:28 +0500

    Задание №8

---
+++ house_8/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все числа от 1 до N.
+
+System.Console.WriteLine("Введите число");
+
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+if(N<=1)
+{
+System.Console.WriteLine("Введи число больше 1");
+}
+else
+{  
+ System.Console.WriteLine("числа от 1 до " +N);
+ while(i<N+1) 
+  {
+  System.Console.Write(i+".");
+  i++; 
+  }
+
+}
+
+
